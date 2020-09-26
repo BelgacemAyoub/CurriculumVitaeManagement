7f0a7714828ac7be2a3679c7c09d8862d4abffb1
Ayoub <belgacem.ayoub8590@gmail.com>
2020-09-26 17:19:49 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
+import { CvComponent } from './cvTech/cv/cv.component';
+import { DetailCvComponent } from './cvTech/detail-cv/detail-cv.component';
+import { ItemCvComponent } from './cvTech/item-cv/item-cv.component';
+import { ListeCvComponent } from './cvTech/liste-cv/liste-cv.component';
+import { FormsModule } from '@angular/forms';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CvComponent,
+    DetailCvComponent,
+    ItemCvComponent,
+    ListeCvComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cvTech/liste-cv/liste-cv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-cv',
+  templateUrl: './liste-cv.component.html',
+  styleUrls: ['./liste-cv.component.css']
+})
+export class ListeCvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
